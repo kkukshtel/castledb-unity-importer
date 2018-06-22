ce9ebf65d5ce23ab95bb7d00558007dd66970769
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2018-06-22 09:59:16 -0400

    working to get list support in

--- Assets/CastleDBImporter/Sample/CDBTest.cs
+++ Assets/CastleDBImporter/Sample/CDBTest.cs
@@ -18,18 +18,18 @@ namespace CastleDBImporter
                 // Debug.Log("wwwwwoowowoow--------------------------------------------------");
                 CastleDB castle = new CastleDB(CastleDB);
                 CastleDB.RootNode root = castle.GenerateDB();
-                unityTest mytest = new unityTest(root.Sheets[0].Lines[0]);
-                Debug.Log("anotherenumcolumn: " + mytest.anotherenumcolumn);
-                Debug.Log("booleanValue: " + mytest.booleanValue);
-                Debug.Log("colorValue: " + mytest.colorValue);
-                Debug.Log("customTypeValue: " + mytest.customTypeValue);
-                Debug.Log("enumValue: " + mytest.enumValue);
-                Debug.Log("fileValue: " + mytest.fileValue);
-                Debug.Log("flagValue: " + mytest.flagValue);
-                Debug.Log("floatValue: " + mytest.floatValue);
-                Debug.Log("imageValue: " + mytest.imageValue);
-                Debug.Log("integerValue: " + mytest.integerValue);
-                Debug.Log("textValue: " + mytest.textValue);
+                // unityTest mytest = new unityTest(root.Sheets[0].Lines[0]);
+                // Debug.Log("anotherenumcolumn: " + mytest.anotherenumcolumn);
+                // Debug.Log("booleanValue: " + mytest.booleanValue);
+                // Debug.Log("colorValue: " + mytest.colorValue);
+                // Debug.Log("customTypeValue: " + mytest.customTypeValue);
+                // Debug.Log("enumValue: " + mytest.enumValue);
+                // Debug.Log("fileValue: " + mytest.fileValue);
+                // Debug.Log("flagValue: " + mytest.flagValue);
+                // Debug.Log("floatValue: " + mytest.floatValue);
+                // Debug.Log("imageValue: " + mytest.imageValue);
+                // Debug.Log("integerValue: " + mytest.integerValue);
+                // Debug.Log("textValue: " + mytest.textValue);
                 test = false;
             }
         }
---
+++ README.md
@@ -0,0 +1,20 @@
+# CastleDB Unity Importer
+Define all your data in the external CastleDB software and enjoy macro-like intellisense for your data inside your code editor
+
+## How it works
+
+## Implementation
+
+## Limitations
+* Currently there is no validation of column names, so column names in CastleDB need to be valid field names in C#. I.E. don't use spaces in your column names, weird characters, etc.
+
+
+## TODO
+* WORKING ON LIST SUPPORT
+* Easier way to add in SimpleJSON than a DLL? Unity's AssemblyBuilder won't take the SimpleJSON reference unless it's passed as a DLL reference. Maybe a namespace issue?
+* Add in List support
+* Add in Color support
+* Figure out a better guide for adding CastleDB Custom Trypes
+* Document way to add in your own CustomType to match with a predefined Type in Unity
+* Rebuilt DLL doesn't have new columns
+* Unclear how references are supposed to work in CastleDB
